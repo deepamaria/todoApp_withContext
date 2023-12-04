5482d12b936b84616c9ed7486972782394f74547
Deepa <deepamaria605@gmail.com>
2023-12-04 19:27:58 +0100

    first commit -created nav,main and footer components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './Footer';
+import Main from './Main';
+import Nav from './Nav';
+
+function App() {
+
+  
+  
+  return (
+    <div className="App">
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
